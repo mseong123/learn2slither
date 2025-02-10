c3b1f5252ff2594fa1e535fb9d2ac7a719801bb5
mseong <mseong123@hotmail.com>
2025-02-10 18:03:34 +0800

    training part for agent

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@
 import random
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 import param
 
 class Agent():
@@ -13,18 +14,20 @@ class Agent():
         # discount factor between 0.9 and 0.99 as per standard implementation
         # and remain constant throughout training
         self._discount: float = discount
-        self._session: int = 0
+        self._session: int = 1
+        self._steps: int = 0
         # use inverse time decay for e greedy algo. Decay scale measures
         # how fast e decays.
         self._decay_scale: int = decay_scale
         self._e: float = 1
         # initialise 2 MLP network (main and target). One is for training,
-        # the other to generate Q target. To prevent chasing a changing objective.
+        # the other to generate Q target. To prevent chasing a changing 
+        # objective.
         self._main_network: MLPRegressor = MLPRegressor(
             hidden_layer_sizes=(10), warm_start=True,
             random_state=param.RANDOM_STATE)
         self._target_network: MLPRegressor = MLPRegressor(
-            hidden_layer_sizes=(10), 
+            hidden_layer_sizes=(10),
             random_state=param.RANDOM_STATE)
         # one hot encode action for input into network
         self._action: list = self._one_hot_encode_action()
@@ -49,11 +52,12 @@ class Agent():
     def _one_hot_encode_action(self) -> list:
         '''return array of one hot encoded action'''
         encoder = OneHotEncoder(sparse_output=False)
-        return encoder.fit_transform([[[param.Action.UP.value],
-                                [param.Action.DOWN.value],
-                                [param.Action.LEFT.value],
-                                [param.Action.RIGHT.value]
-                                ]])
+        return encoder.fit_transform(
+            [[[param.Action.UP.value],
+              [param.Action.DOWN.value],
+              [param.Action.LEFT.value],
+              [param.Action.RIGHT.value]
+              ]])
 
     def _choose_action(self, info: list) -> int:
         '''returns an action based on exploration/exploitation
@@ -70,24 +74,46 @@ class Agent():
             # if exploitation, predict a list of 4 Q_target values for
             # each action and choose the max
             if initial is True:
-                q_target: list = [self._target_network.predict(
-                    info[0].append(*self._action[action.value]))
-                    for action in list(param.Action)]
-                action = q_target.index(max(q_target))
-                self._replay_buffer.append(info[0])
+                action = self._max_q_value(info[0])
+                self._replay_buffer.append([info[0]])
             else:
-                q_target: list = [self._target_network.predict(
-                                  info[3].append(*self._action[action.value]))
-                                  for action in list(param.Action)]
-                action = q_target.index(max(q_target))
-                self._replay_buffer.append(info[3])
+                action = self._max_q_value(info[3]) 
+                self._replay_buffer.append([info[3]])
                 self._replay_buffer[(len(self._replay_buffer)
                                      - 2)].extend(info)
+                if info[2] is True:
+                    self._session += 1
         return action
 
+    def _max_q_value(self, state: list) -> float:
+        '''function to calculate max_q_value for all actions'''
+        q_target: list = [self._target_network.predict(
+                           state.append(*self._action[action.value]))
+                           for action in list(param.Action)]
+        return q_target.index(max(q_target))
 
-    def _train() -> None:
-       '''batch sample training from replay buffer''' 
+    def _train_one(self):
+        '''training methodology for < 10 session'''
+        # 1) pop past experiences if exceed threshold
+        if len(self._replay_buffer > param.REPLAY_SIZE_ONE):
+            del self._replay_buffer[0]
+        # 2) train every step for session < 10
+        # x = input state (list)
+        x = [info[0] for info in self._replay_buffer]
+        # y = reward + future reward
+        y = [(info[1] + (self._discount * self._max_q_value(info[3])))
+             for info in self._replay_buffer]
+        self._main_network.fit(x, y)
+        # 3) 
+
+
+    def _train(self) -> None:
+       '''batch sample training from replay buffer. Training methodology 
+       changes incrementally based on total no. of sessions run by model 
+       (1, 10, 100) based on subject pdf'''
+       if self._session < 10:
+           self._train_one()
+           
 
     def action(self, info: list) -> int:
         '''outputs an action based on info passed by environment
@@ -100,10 +126,11 @@ class Agent():
         if self._session == 0:
             self._target_network.fit(input_state, [0])
         action: int = self._choose_action(info)
-        self. 
+        self._train()
         # increment permanent session count by 1 for agent for training
         # metrics
-        self._session += 1
+        self._steps += 1
+        return action
 
 
 
