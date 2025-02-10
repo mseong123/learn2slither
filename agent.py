eb8292f23a60c7db3edc42593f02f9501676e858
mseong <mseong123@hotmail.com>
2025-02-10 16:22:53 +0800

    writing training function now

--- agent.py
+++ agent.py
@@ -20,11 +20,16 @@ class Agent():
         self._e: float = 1
         # initialise 2 MLP network (main and target). One is for training,
         # the other to generate Q target. To prevent chasing a changing objective.
-        self._main_network: MLPRegressor = MLPRegressor(hidden_layer_sizes=(10), warm_start=True,
-                                             random_state=param.RANDOM_STATE)
-        self._target_network: MLPRegressor = MLPRegressor(**self._main_network.get_params())
+        self._main_network: MLPRegressor = MLPRegressor(
+            hidden_layer_sizes=(10), warm_start=True,
+            random_state=param.RANDOM_STATE)
+        self._target_network: MLPRegressor = MLPRegressor(
+            hidden_layer_sizes=(10), 
+            random_state=param.RANDOM_STATE)
         # one hot encode action for input into network
         self._action: list = self._one_hot_encode_action()
+        # replay buffer would have [[state], action, reward, terminal bool,
+        # [next_state]] state
         self._replay_buffer: list = []
 
     @property
@@ -50,40 +55,55 @@ class Agent():
                                 [param.Action.RIGHT.value]
                                 ]])
 
-    def choose_action(self, info: list) -> int:
-        '''outputs an action based on info passed by environment'''
+    def _choose_action(self, info: list) -> int:
+        '''returns an action based on exploration/exploitation
+        and store info in replay buffer'''
+        action: int = 0
+        # initial state when game is initiated (no prev state)
+        initial: bool = True if len(info) == 1 else False
         # exploration vs exploitation
         if random.random() < self._e:
-            return random.randint(list(param.Action)[0].value,
-                                  len(param.Action) - 1)
+            # if exploration choose random action integer
+            action = random.randint(list(param.Action)[0].value,
+                                    len(param.Action) - 1)
         else:
-            # initial state when game is initiated (no prev state)
-            if len(info) == 1:
-                # set up initial weights for target network if session = 0 in
-                # agent with initial inputs and first action and 0 result.
-                # Step required so can start generating Q target values
-                # although model is untrained
-                input: list = info[0].append(self._action[0])
-                if self._session == 0:
-                    self._target_network.fit(input, [0])
-                # predict a list of 4 Q_target values for each action
+            # if exploitation, predict a list of 4 Q_target values for
+            # each action and choose the max
+            if initial is True:
                 q_target: list = [self._target_network.predict(
                     info[0].append(*self._action[action.value]))
-                                  for action in list(param.Action)]
-                # append info to replay buffer
-                self._replay_buffer.append(info[1])
-                return q_target.index(max(q_target))
-            # subsequent states after action is made
+                    for action in list(param.Action)]
+                action = q_target.index(max(q_target))
+                self._replay_buffer.append(info[0])
             else:
                 q_target: list = [self._target_network.predict(
-                    info[3].append(*self._action[action.value]))
+                                  info[3].append(*self._action[action.value]))
                                   for action in list(param.Action)]
-                # append new info to new replay buffer
-                # and previous replay buffer
+                action = q_target.index(max(q_target))
                 self._replay_buffer.append(info[3])
-                self._replay_buffer[
-                    (len(self._replay_buffer) - 2)].extend(info)
-                return q_target.index(max(q_target))
+                self._replay_buffer[(len(self._replay_buffer)
+                                     - 2)].extend(info)
+        return action
+
+
+    def _train() -> None:
+       '''batch sample training from replay buffer''' 
+
+    def action(self, info: list) -> int:
+        '''outputs an action based on info passed by environment
+        and append info in replay buffer for training'''
+        # set up initial weights for target network if session = 0 in
+        # agent with initial inputs and first action and 0 result.
+        # Step required so can start generating Q target values
+        # although model is untrained
+        input_state: list = info[0].append(self._action[0])
+        if self._session == 0:
+            self._target_network.fit(input_state, [0])
+        action: int = self._choose_action(info)
+        self. 
+        # increment permanent session count by 1 for agent for training
+        # metrics
+        self._session += 1
 
 
 
